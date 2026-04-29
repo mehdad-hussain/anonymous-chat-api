5fe0464c314aa24f2c08d767bfbc1286cc7ca53e
mehdad-hussain <hussain.mehdad1@gmail.com>
2026-04-29 21:27:00 +0600

    [feat]: add redis module with ioredis client provider

---
+++ src/redis/redis.module.ts
@@ -0,0 +1,27 @@
+import { Module, Global } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import Redis from 'ioredis';
+
+export const REDIS_CLIENT = Symbol('REDIS_CLIENT');
+
+@Global()
+@Module({
+  providers: [
+    {
+      provide: REDIS_CLIENT,
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const redisUrl = configService.get<string>('REDIS_URL');
+        if (!redisUrl) {
+          throw new Error('REDIS_URL is not defined');
+        }
+        return new Redis(redisUrl, {
+          maxRetriesPerRequest: 3,
+          retryStrategy: (times: number) => Math.min(times * 100, 3000),
+        });
+      },
+    },
+  ],
+  exports: [REDIS_CLIENT],
+})
+export class RedisModule {}
